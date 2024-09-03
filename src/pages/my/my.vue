4f5ec001fa96dac27ad65fcf44b87e592c5f3caf
szy <190977024@qq.com>
2024-09-03 15:21:44 +0800

    拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-
+import {http} from '@/utils/http'
+const getData = async() =>{
+  const res = await http ({
+    method:'GET',
+    // url:'/home/banner',
+    // url:'/member/profile',
+    url:'',
+    header:{},
+  })
+}
 const memberStore = useMemberStore()
 </script>
 
@@ -11,6 +20,7 @@ const memberStore = useMemberStore()
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token:'123456'
         })
       "
       size="mini"
@@ -20,6 +30,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" plain type="primary">测试请求</button>
   </view>
 </template>
 
